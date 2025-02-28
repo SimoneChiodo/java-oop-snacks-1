8740fcd97e1e5373116e9037857e11620d7c8560
SimoneChiodo <simochi.005@gmail.com>
2025-02-28 16:45:27 +0100

    Add Snack-1

---
+++ src/org/lessons/snack/java/Main.java
@@ -0,0 +1,11 @@
+package org.lessons.snack.java;
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Snack 1
+    Studente student = new Studente("Francesco", "Rossi", 16);
+    System.err.println(student.getStudente());;
+
+  }
+}
--- src/org/lessons/snack/java/Studente.java
+++ src/org/lessons/snack/java/Studente.java
@@ -2,4 +2,18 @@ package org.lessons.snack.java;
 
 public class Studente {
 
+  private String name;
+  private String surname;
+  private int age;
+
+  Studente(String name, String surname, int age){
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+  }
+
+  String getStudente(){
+    return "Studente: " + name + " " + surname + ", " + age + " anni";
+  }
+
 }
